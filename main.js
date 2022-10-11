9616d490a72b367dabcba446e871560fe9f8e06c
Justin Williams <justinwilliams@Justins-MacBook-Pro-3.local>
2022-10-11 16:48:32 -0400

    Button functionality

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let color = "black"
+
+
 function populateBoard(size) {
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll('div');
@@ -8,10 +11,8 @@ function populateBoard(size) {
     let amount = size * size;
     for (let i = 0; i < amount; i++) {
         let square = document.createElement("div");
-        square.addEventListener('mouseover', () => {
-            square.style.background = "black";
-        })
-        square.style.backgroundColor = "blue";
+        square.addEventListener('mouseover', colorSquare);
+        square.style.backgroundColor = "gray";
         board.insertAdjacentElement("beforeend", square);
     }
 }
@@ -24,5 +25,17 @@ function changeSize(input) {
     } else {
         console.log('too many squares');
     }
-    
+
+}
+
+function colorSquare() {
+    if(color = 'random') {
+        this.style.backgroundColor = `hs1(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
 }
