c067a333f2b6114f31a886c67d0f5fad788d1ed3
Justin Williams <justinwilliams@Justins-MacBook-Pro-3.local>
2022-10-11 14:01:49 -0400

    Adding javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+function populateBoard(size)
+let board = document.querySelector('.board')
+board.getElementsByClassName.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.getElementsByClassName.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+for(let i = 0; i<256; i++) {
+    let square = document.createElement('div');
+    square.style.backgroundColor = 'blue';
+    board.insertAdjacentElement('beforeend', square);
+}
